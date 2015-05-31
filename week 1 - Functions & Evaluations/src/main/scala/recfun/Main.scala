d4f4a1b3f57b0b705c60c73965cd0009a0ef8dce
dgroup <ydantezs@yandex.ru>
2015-05-31 23:16:26 +0100

    Week 1 started.

---
+++ week 1 - Functions & Evaluations/src/main/scala/recfun/Main.scala
@@ -0,0 +1,40 @@
+package recfun
+
+object Main {
+
+    def main(args: Array[String]) {
+        println("Pascal's Triangle")
+        for (row <- 0 to 10) {
+            for (col <- 0 to row)
+                print(pascal(col, row) + " ")
+            println()
+        }
+
+        println( balance(":-)" toList ) )
+    }
+
+
+    /**
+     * Exercise 1
+     */
+    def pascal(c: Int, r: Int):Int = if (c == 0 || c == r) 1 else pascal(c-1, r-1) + pascal(c, r-1)
+
+
+    /**
+    * Exercise 2
+    */
+    def balance(chars: List[Char]): Boolean = {
+        if (chars.isEmpty || !chars.contains('(')) return false
+        for(c <- chars if c == '(' ) {
+            val tail = chars.tail
+            if (!tail.contains(')')) return false
+            return if (tail.contains('(')) balance(tail) else true
+        }
+        false
+    }
+
+    /**
+    * Exercise 3
+    */
+    def countChange(money: Int, coins: List[Int]): Int = 0
+}
